389af8aabc6c312d44a233ef5de0c8f8a70963a5
linagotye <lincha2403@gmail.com>
2023-03-30 21:19:18 -0400

    the basic structure

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BasicStructure audi=new BasicStructure();
+        audi.color="blue";
+        audi.model="A3";
+        audi.power="344lc";
+    }
+}
