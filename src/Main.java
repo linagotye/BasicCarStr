91922e5ce4c62d14f4182f6d371775a8615f0f43
linagotye <lincha2403@gmail.com>
2023-03-30 21:57:29 -0400

    engine added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         audi.color="blue";
         audi.model="A3";
         audi.power="344lc";
+
+        Engine x = new Engine();
+        x.cylinders="8";
+        x.size="18181";
+        x.weight="111";
     }
 }
